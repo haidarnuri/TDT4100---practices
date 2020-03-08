ca394cfc4dd310ecd611c2070e39c521f8468a0b
Haidar Nuri <haidarnuri@gmail.com>
2020-03-08 20:00:17 +0100

    added first method to test NameComaprator

--- Practice6/src/test/java/interfaces/NamedComparatorTest2.java
+++ Practice6/src/test/java/interfaces/NamedComparatorTest2.java
@@ -6,53 +6,32 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 
 
 @SuppressWarnings("all")
 public class NamedComparatorTest2 extends TestCase {
 
-    private Named name1;
-    private Named name2;
-    private Named name3;
+    private Named name1 = new Person2("Halvor Hansen");
+    private Named name2 = new Person2("Hans Pettersen");
+    private Named name3 = new Person2("Haidar Nuri");
     private Named name4;
     private Named name5;
     private Named name6;
 
-    private ArrayList<Named> listSameFamilyName;
-
-    @Before
-    public void init(){
-        //First initiate two objects with different familyname
-        name1.setFullName("Halvor Hansen");
-        name2.setFullName("Hans Pettersen");
-
-
-        //First initiate two objects with equal familyname and different given name
-        name3.setFullName("Hans Pettersen");
-        name4.setFullName("Paul Hansen");
-
-        //equal last name and given name
-        name5.setFullName("Haidar Nuri");
-        name6.setFullName("Haidar Nuri");
-
-        listSameFamilyName = null;
-    }
+    private ArrayList<Named> listSameFamilyName = new ArrayList<>();
+    private ArrayList<Named> actualtList = new ArrayList<>();
 
     @Test
-    public void testt(){
-        try {
-            listSameFamilyName.add(name1);
-            listSameFamilyName.add(name2);
-            failNotEquals("hehehe", Collections.sort(listSameFamilyName,NamedComparator),[name1.getFamilyName(),name2.getFamilyName()])
-        }
-        catch (Exception e){
-
-        }
-
-
+    public void test1(){
+        listSameFamilyName.add(name2);
+        listSameFamilyName.add(name1);
+        listSameFamilyName.add(name3);
+        actualtList.add(name1);
+        actualtList.add(name3);
+        actualtList.add(name2);
+        Collections.sort(listSameFamilyName,new NamedComparator());
+        assertEquals(listSameFamilyName, actualtList);
     }
-
 
 }
