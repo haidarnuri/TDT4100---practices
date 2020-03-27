dbd4ace3d3bb314ee1613d3b87b4eba83c29826d
haidarnuri <haidarnuri@gmail.com>
2020-03-27 09:59:30 +0100

    completed ForeldreSpar.java

--- Practice9/src/main/java/inheritance/ForeldreSpar.java
+++ Practice9/src/main/java/inheritance/ForeldreSpar.java
@@ -1,13 +1,31 @@
 package inheritance;
 
 public class ForeldreSpar extends SavingsAccount {
+    private int remainingWithdrawals=0;
+    private int legalWithdrawal;
 
-    ForeldreSpar(double dewdwe, int temp) {
-        super(dewdwe);
+    ForeldreSpar(double interest, int legalWithdrawal) {
+        super(interest);
+        this.legalWithdrawal=legalWithdrawal;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if(getRemainingWithdrawals()==0) {
+            throw new IllegalStateException("Can not do any more withdrawals");
+        }
+        super.withdraw(amount);
+        this.remainingWithdrawals+=1;
+    }
+
+    @Override
+    public void endYearUpdate() {
+        super.endYearUpdate();
+        this.remainingWithdrawals=0;
     }
 
     public int getRemainingWithdrawals(){
-        return 0;
+        return this.legalWithdrawal-this.remainingWithdrawals;
     }
 
 }
